2f1a5343c0cc0126d2ba96e4e6fb49df94b4463d
cnbailian <594647004@qq.com>
2021-02-22 09:18:51 +0800

    4.2.3 自动生成完整的 RPC 代码

---
+++ rpc/4.2.3/service.go
@@ -0,0 +1,71 @@
+/*
+Copyright 2021 BaiLian.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+const tmplService = `
+{{$root := .}}
+
+type {{.ServiceName}}Interface interface {
+    {{- range $_, $m := .MethodList}}
+    {{$m.MethodName}}(*{{$m.InputTypeName}}, *{{$m.OutputTypeName}}) error
+    {{- end}}
+}
+
+func Register{{.ServiceName}}(
+    srv *rpc.Server, x {{.ServiceName}}Interface,
+) error {
+    if err := srv.RegisterName("{{.ServiceName}}", x); err != nil {
+        return err
+    }
+    return nil
+}
+
+type {{.ServiceName}}Client struct {
+    *rpc.Client
+}
+
+var _ {{.ServiceName}}Interface = (*{{.ServiceName}}Client)(nil)
+
+func Dial{{.ServiceName}}(network, address string) (
+    *{{.ServiceName}}Client, error,
+) {
+    c, err := rpc.Dial(network, address)
+    if err != nil {
+        return nil, err
+    }
+    return &{{.ServiceName}}Client{Client: c}, nil
+}
+
+{{range $_, $m := .MethodList}}
+func (p *{{$root.ServiceName}}Client) {{$m.MethodName}}(
+    in *{{$m.InputTypeName}}, out *{{$m.OutputTypeName}},
+) error {
+    return p.Client.Call("{{$root.ServiceName}}.{{$m.MethodName}}", in, out)
+}
+{{end}}
+`
+
+type ServiceSpec struct {
+	ServiceName string
+	MethodList  []ServiceMethodSpec
+}
+
+type ServiceMethodSpec struct {
+	MethodName     string
+	InputTypeName  string
+	OutputTypeName string
+}
